86594a084ec88d286ae1b32b9bc53edd344041bf
Oscar Velilla <o.velillaa@gmail.com>
2025-04-03 22:22:31 +0200

    updates

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -14,37 +14,37 @@ export default getRequestConfig(async ({ requestLocale }) => {
   const messages = {
     ...(
       await import(
-        `../../app/[locale]/(auth)/(pages)/signin/messages/${locale}.json`
+        `../app/[locale]/(auth)/(pages)/signin/messages/${locale}.json`
       )
     ).default,
     ...(
       await import(
-        `../../app/[locale]/(auth)/(pages)/signup/messages/${locale}.json`
+        `../app/[locale]/(auth)/(pages)/signup/messages/${locale}.json`
       )
     ).default,
     ...(
       await import(
-        `../../app/[locale]/(auth)/(pages)/forgot-password/messages/${locale}.json`
+        `../app/[locale]/(auth)/(pages)/forgot-password/messages/${locale}.json`
       )
     ).default,
     ...(
       await import(
-        `../../app/[locale]/(auth)/(pages)/reset-password/messages/${locale}.json`
+        `../app/[locale]/(auth)/(pages)/reset-password/messages/${locale}.json`
       )
     ).default,
     ...(
       await import(
-        `../../app/[locale]/(auth)/(pages)/verify-email/messages/${locale}.json`
+        `../app/[locale]/(auth)/(pages)/verify-email/messages/${locale}.json`
       )
     ).default,
     ...(
       await import(
-        `../../app/[locale]/(auth)/(pages)/signout/messages/${locale}.json`
+        `../app/[locale]/(auth)/(pages)/signout/messages/${locale}.json`
       )
     ).default,
     ...(
       await import(
-        `../../app/[locale]/(auth)/(pages)/auth-error/messages/${locale}.json`
+        `../app/[locale]/(auth)/(pages)/auth-error/messages/${locale}.json`
       )
     ).default,
   };
